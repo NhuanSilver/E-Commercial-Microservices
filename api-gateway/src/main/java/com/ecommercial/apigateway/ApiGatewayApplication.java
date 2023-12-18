99e39c7b613c9a458d738833a45f5b1d139b248a
NhuanSilver <nhuan.nhp@gmail.com>
2023-12-18 10:39:48 +0700

    update config

--- api-gateway/src/main/java/com/ecommercial/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ecommercial/apigateway/ApiGatewayApplication.java
@@ -3,11 +3,9 @@ package com.ecommercial.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableWebSecurity
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
